adbed301c2309c219c603b14823eaf5141f2b3c0
Azer3012 <azer0297206@gmail.com>
2021-06-30 00:22:13 +0400

    fetching data

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,23 +1,48 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
-    <router-link :to="{ name: 'JobDetail', params: { id: job.id } }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div class="jobs" v-for="job in jobs" :key="job.id">
+      <router-link :to="{ name: 'JobDetail', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loadind jobs....</p>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      jobs: [
-        { id: 1, title: "UX Designer", details: "lorem" },
-        { id: 2, title: "web Developer", details: "lorem" },
-        { id: 3, title: "UX Vue Developer", details: "lorem" },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
 <style>
+.jobs{
+  width: 50%;
+  margin: 0 auto;
+}
+.jobs a{
+  color: black;
+  display: block;
+  text-decoration: none;
+  background-color: #ddd;
+  padding: 5px 0;
+  margin-bottom: 5px;
+  transition: 0.5s;
+
+}
+.jobs a:hover{
+  background: rgb(250, 247, 247);
+  color: #42b983;
+}
 </style>
